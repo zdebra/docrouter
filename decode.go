ccb15b639b5cc243e9c24320594e871d32a2d4a4
zdebra <zd.brabec@gmail.com>
2021-08-03 22:21:06 +0200

    decode ref

--- decode.go
+++ decode.go
@@ -27,54 +27,62 @@ func DecodeParams(structPtr interface{}, req *http.Request) error {
 	}
 
 	for _, tField := range pParam.fields {
-		paramName := tField.getTagName()
+		paramName, paramKind := tField.getTagName(), tField.getTagKind()
 		if paramName == "" {
 			continue
 		}
 
-		valueStr := ""
-		switch tField.getTagKind() {
-		case openapi3.ParameterInQuery:
-			valueStr = req.URL.Query().Get(paramName)
-		case openapi3.ParameterInPath:
-			x, found := mux.Vars(req)[paramName]
-			if !found {
-				return fmt.Errorf("parameter %q not found in path", paramName)
-			}
-			valueStr = x
-		default:
-			return fmt.Errorf("paramter kind %q not supported", tField.getTagKind())
+		valueStr, err := strValueFromRequest(paramName, paramKind, req)
+		if err != nil {
+			return fmt.Errorf("read string value from request: %v", err)
 		}
 
-		structField := sElem.FieldByName(tField.name)
-		if !structField.IsValid() {
-			return fmt.Errorf("invalid field name %q", tField.name)
+		if err := convertAndSetStructField(&sElem, tField.name, valueStr); err != nil {
+			return fmt.Errorf("convert and set struct field: %w", err)
 		}
-
-		if !structField.CanSet() {
-			return fmt.Errorf("can't set field %q", tField.name)
-		}
-
-		switch structField.Kind() {
-		case reflect.Int:
-			intVal, err := strconv.Atoi(valueStr)
-			if err != nil {
-				return fmt.Errorf("converting %q to int: %v", valueStr, err)
-			}
-			structField.SetInt(int64(intVal))
-		case reflect.Bool:
-			boolVal, err := strconv.ParseBool(valueStr)
-			if err != nil {
-				return fmt.Errorf("converting %q to bool: %v", valueStr, err)
-			}
-			structField.SetBool(boolVal)
-		case reflect.String:
-			structField.SetString(valueStr)
-		default:
-			return fmt.Errorf("unsupported conversion for %v", structField.Kind())
-		}
-
 	}
 
+	return nil
+}
+
+func strValueFromRequest(paramName, kind string, req *http.Request) (string, error) {
+	switch kind {
+	case openapi3.ParameterInQuery:
+		return req.URL.Query().Get(paramName), nil
+	case openapi3.ParameterInPath:
+		return mux.Vars(req)[paramName], nil
+	default:
+		return "", fmt.Errorf("paramter kind %q not supported", kind)
+	}
+}
+
+func convertAndSetStructField(sVal *reflect.Value, fieldName, valueStr string) error {
+	structField := sVal.FieldByName(fieldName)
+	if !structField.IsValid() {
+		return fmt.Errorf("invalid field name %q", fieldName)
+	}
+
+	if !structField.CanSet() {
+		return fmt.Errorf("can't set field %q", fieldName)
+	}
+
+	switch structField.Kind() {
+	case reflect.Int:
+		intVal, err := strconv.Atoi(valueStr)
+		if err != nil {
+			return fmt.Errorf("converting %q to int: %v", valueStr, err)
+		}
+		structField.SetInt(int64(intVal))
+	case reflect.Bool:
+		boolVal, err := strconv.ParseBool(valueStr)
+		if err != nil {
+			return fmt.Errorf("converting %q to bool: %v", valueStr, err)
+		}
+		structField.SetBool(boolVal)
+	case reflect.String:
+		structField.SetString(valueStr)
+	default:
+		return fmt.Errorf("unsupported conversion for %v", structField.Kind())
+	}
 	return nil
 }
